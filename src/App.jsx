be17eb515a2c3061cf1b01c91fce767f8dba5cf3
FedericoBellezza <federico.bellezza2001@gmail.com>
2024-12-10 17:29:09 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./Pages/HomePage";
+import AboutPage from "./Pages/AboutPage";
+import PostListPage from "./Pages/PostListPage";
+
+function App() {
+  return (
+    <>
+      <h1 className="card m-3 p-3 d-inline-block">Hello World</h1>
+    </>
+  );
+}
+
+export default App;
