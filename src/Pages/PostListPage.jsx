65cc3e209b33e2c2e217a0629c381f34764263a6
FedericoBellezza <federico.bellezza2001@gmail.com>
2024-12-11 15:02:08 +0100

    import the posts list fromthe API

--- src/Pages/PostListPage.jsx
+++ src/Pages/PostListPage.jsx
@@ -1,3 +1,21 @@
+import { useEffect, useState } from "react";
+
 export default function PostListPage() {
-  return <h1>Lista dei post</h1>;
+  const apiUrl = import.meta.env.API_URL;
+  // console.log(import.meta.env.API_URL);
+  const [posts, setPost] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:3000/posts")
+      .then((res) => res.json())
+      .then((data) => {
+        setPost(data.posts);
+      });
+  }, []);
+  console.log(posts);
+
+  return (
+    <>
+      <h1>Lista dei post</h1>
+    </>
+  );
 }
