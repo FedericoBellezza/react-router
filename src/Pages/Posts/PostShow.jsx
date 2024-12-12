9c263e727d55dffe277d4ebfcd6adfad6271fd54
FedericoBellezza <federico.bellezza2001@gmail.com>
2024-12-12 17:42:49 +0100

    fix "tags" fetch bug

--- src/Pages/Posts/PostShow.jsx
+++ src/Pages/Posts/PostShow.jsx
@@ -4,30 +4,32 @@ import { useParams } from "react-router-dom";
 export default function PostShow() {
   const apiUrl = import.meta.env.VITE_API_URL;
   const postId = useParams().id;
-  const [post, setPost] = useState([]);
+  const [postShow, setPostShow] = useState([]);
 
   useEffect(() => {
     const url = `${apiUrl}/posts/${postId}`;
     fetch(url)
       .then((res) => res.json())
       .then((data) => {
-        console.log(data);
-        setPost(data);
+        setPostShow(data);
         console.log(url);
       });
   }, []);
 
   return (
     <>
-      <h1>Dettaglio Post {postId}</h1>
+      <h1>Dettaglio Post {postShow.id}</h1>
       <div className="card mt-5 mb-5 w-50">
-        <img src={post.img} className="card-img-top" />
+        <img src={postShow.img} className="card-img-top" />
         <div className="card-body">
-          <h5 className="card-title">{post.title}</h5>
-          <p className="card-text">{post.content}</p>
-          {post.tags.map((tag) => {
-            return <p className="badge text-bg-secondary me-1">{tag}</p>;
-          })}
+          <h5 className="card-title">{postShow.title}</h5>
+          <p className="card-text">{postShow.content}</p>
+          {Array.isArray(postShow.tags) &&
+            postShow.tags.map((tag, index) => (
+              <span className="badge text-bg-secondary me-1" key={index}>
+                {tag}
+              </span>
+            ))}
         </div>
       </div>
     </>
